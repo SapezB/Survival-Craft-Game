6140b4c73c70ca05f99a03ef93b225d205eb2021
Ben <65823405+SapezB@users.noreply.github.com>
2024-01-08 03:05:07 +0000

    crafting code

--- Assets/Scripts/Invetory/InventorySystem.cs
+++ Assets/Scripts/Invetory/InventorySystem.cs
@@ -4,18 +4,13 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class InventorySystem : MonoBehaviour {
+public class InventorySystem {
     
     [SerializeField] private List<Slot> allInventorySlots;
    
     [SerializeField] private InventorySystem hotbarSlots;
-    public List<Recipe> itemRecipes = new List<Recipe>();
-    public GameObject player;
-
-
     
-
-
+    
 
 
     public List<Slot> slots => allInventorySlots;
@@ -78,72 +73,5 @@ public class InventorySystem : MonoBehaviour {
         return freeSlot == null ? false : true;
     }
 
-    //Crafting
-    public void craftItem(string itemName){
-        foreach(Recipe recipe in itemRecipes){
-            if(recipe.createdItem.GetComponent<ItemData>().name == itemName)
-            {
-                bool haveAllIngredients = true;
-                for(int i = 0; i < recipe.requiredIngredients.Count; i++){
-                    if(haveAllIngredients){
-                        haveAllIngredients = haveIngredient(recipe.requiredIngredients[i].itemName, recipe.requiredIngredients[i].amountNeeded);
-                    }
-                }
 
-                if(haveAllIngredients){
-                    for(int i = 0; i < recipe.requiredIngredients.Count; i++){
-                        removeIngredient(recipe.requiredIngredients[i].itemName, recipe.requiredIngredients[i].amountNeeded);
-                    }
-                    GameObject craftedItem = Instantiate(recipe.createdItem, player.transform.position, Quaternion.identity);
-                    craftedItem.GetComponent<ItemData>().currentQuantity = recipe.amountMade;
-
-                    AddToInventory(craftedItem.GetComponent<ItemData>(),craftedItem.GetComponent<ItemData>().currentQuantity);
-                }
-
-                break;
-            }
-        }
-    }
-
-    private void removeIngredient(string itemName, int amount){
-        if(!haveIngredient(itemName,amount)){
-            return;
-        }
-
-        int remainingAmount = amount;
-
-        foreach(Slot curslot in allInventorySlots){
-            ItemData item = curslot.getItem();
-
-            if(item != null && item.name == itemName){
-                if(item.currentQuantity >= remainingAmount){
-                    item.currentQuantity -= remainingAmount;
-
-                    if(item.currentQuantity == 0){
-                        curslot.ClearSlot();
-                    }
-                    return;
-                }
-                else{
-                    remainingAmount -= item.currentQuantity;
-                    curslot.ClearSlot();
-                }
-            }
-
-        }
-    }
-
-    private bool haveIngredient(string itemName, int amount){
-        int foundAmount = 0;
-        foreach(Slot curslot in allInventorySlots){
-            if(curslot.hasItem() && curslot.getItem().name == itemName){
-                foundAmount += curslot.getItem().currentQuantity;
-                
-                if(foundAmount >= amount){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
 }
